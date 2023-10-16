3e67c62d07d35b80f65117a196d744251926e61e
lunuj <68381451+lunuj@users.noreply.github.com>
2023-10-16 16:06:33 +0800

    ✨ feat(SPI通信模块): 添加使用STM32硬件SPI通信
    使用STM32自带硬件电路实现SPI通信,构建SPI初始化、开始通信、结束通信、交换字节的时序

--- Hardware/MySPI.c
+++ Hardware/MySPI.c
@@ -1,6 +1,7 @@
 #include "stm32f10x.h"
 #include "MySPI.h"
 
+//软件实现SPI通信
 /**
  * @brief SPI通信初始化
  * @retval 无
@@ -96,4 +97,80 @@ u8 MySPI_SwapByte(u8 ByteSwap){
         MySPI_W_SCK(0);
     }
     return Data;
+}
+
+//硬件实现SPI通信
+
+void MySPIH_Init(void){
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    SPI_InitTypeDef SPI_InitStructure;
+    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+    SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_128;
+    SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+    SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+    SPI_InitStructure.SPI_CRCPolynomial = 7;
+    SPI_Init(SPI1, &SPI_InitStructure);
+
+    SPI_Cmd(SPI1,ENABLE);
+
+    MySPIH_W_SS(1);
+}
+
+/**
+ * @brief SPI写SS
+ * @param {u8} BitValue 修改SS数值0为低电平其他为高电平
+ * @retval 无
+ */
+void MySPIH_W_SS(u8 BitValue){
+    GPIO_WriteBit(GPIOA, MySPI_SS, (BitAction)BitValue);
+}
+
+/**
+ * @brief SPI发送开始状态
+ * @retval 无
+ */
+void MySPIH_Start(){
+    MySPIH_W_SS(0);
+}
+
+/**
+ * @brief SPI发送结束状态
+ * @retval 无
+ */
+void MySPIH_Stop(){
+    MySPIH_W_SS(1);
+}
+
+/**
+ * @brief SPI交换一字节数据
+ * @param {u8} ByteSwap 交换出的一字节数据
+ * @retval {u8} 交换入的一字节数据
+ */
+u8 MySPIH_SwapByte(u8 ByteSwap){
+    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
+    SPI_I2S_SendData(SPI1,ByteSwap);
+    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET);
+    return SPI_I2S_ReceiveData(SPI1);
 }
--- Hardware/MySPI.h
+++ Hardware/MySPI.h
@@ -1,25 +1,26 @@
 #ifndef __MYSPI_H__
 #define __MYSPI_H__
 
-#define MySPI_SCK GPIO_Pin_0
-#define MySPI_SS GPIO_Pin_1
-#define MySPI_MISO GPIO_Pin_10
-#define MySPI_MOSI GPIO_Pin_11
+#define MySPI_SCK GPIO_Pin_6
+#define MySPI_SS GPIO_Pin_4
+#define MySPI_MISO GPIO_Pin_5
+#define MySPI_MOSI GPIO_Pin_7
 
-
+//软件实现SPI
 void MySPI_Init(void);
-
 void MySPI_W_SS(u8 BitValue);
-
 void MySPI_W_SCK(u8 BitValue);
-
 u8 MySPI_R_MISO(void);
-
 void MySPI_Start();
-
 void MySPI_Stop();
-
 u8 MySPI_SwapByte(u8 ByteSwap);
+
+//硬件实现SPI
+void MySPIH_Init(void);
+void MySPIH_W_SS(u8 BitValue);
+void MySPIH_Start();
+void MySPIH_Stop();
+u8 MySPIH_SwapByte(u8 ByteSwap);
 
 #endif // __MYSPI_H__
 
